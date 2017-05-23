ada6099894e5673d22748c4b5d3704c86f5703af
goodmit <dymbers@mail.ru>
2017-05-23 22:18:41 +0300

    -Добавил todo лист, чтобы не забывать, что еще надо сделать.

--- src/TestServer.java
+++ src/TestServer.java
@@ -5,21 +5,29 @@ import server.Server;
 public class TestServer {
 
     //public static final String SERVER_HOST = "localhost"; //127.0.0.1
-    public static final int SERVER_PORT = 9933;
-    public static final String DB_NAME = "chat.db";
+    private static final int SERVER_PORT = 9933;
+    private static final String DB_NAME = "chat.db";
 
     public static void main(String[] args) {
         Server server = new Server(SERVER_PORT, DB_NAME);
 
         //чтобы основной поток не "замирал" в этом месте
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                server.waitForClient();
-            }
-        }).start();
+        new Thread(server::waitForClient).start();
         server.addFilter(new JavaOnlyFilter());
         server.addFilter(new ChairOnlyFilter());
         //и т.д. - по ходу программы можем добавлять новые фильтры
+    }
+
+    private void commandHandle(String command) {
+        switch (command) {
+            case "stop":
+                break;
+            case "game":
+                break;
+            case "status":
+                break;
+            default:
+                break;
+        }
     }
 }
